d58250f98e9efc19053a16b5f58a5d7f4d5a8df1
piotrek <piotr.radecki00@gmail.com>
2023-01-27 17:35:17 +0100

    dodanie 2 droga

--- SO/MainWindow.xaml.cs
+++ SO/MainWindow.xaml.cs
@@ -39,8 +39,10 @@ namespace SO
         List<double> ya2 = new List<double>();
         List<double> xa22 = new List<double>();
         List<double> ya22 = new List<double>();
+        List<double> v2 = new List<double>();
+        List<double> v22 = new List<double>();
 
-        List<Rectangle> train = new List<Rectangle>();
+        List<Rectangle> trains = new List<Rectangle>();
         List<double> yac = new List<double>();
         bool stop;
 
@@ -54,11 +56,11 @@ namespace SO
             Thread droga1 = new Thread(Droga1);
             droga1.Start();
 
-            //Thread auto2 = new Thread(Auto2);
-            //auto2.Start();
+            Thread auto2 = new Thread(Auto2);
+            auto2.Start();
 
-            //Thread droga2 = new Thread();
-            // droga2.Start();
+            Thread droga2 = new Thread(Droga2);
+            droga2.Start();
 
             Thread pociag = new Thread(Pociag);
             pociag.Start();
@@ -93,7 +95,7 @@ namespace SO
                 Thread.Sleep(1500);
             }
         }
-        /* public void Auto2()
+         public void Auto2()
          {
              while (true)
              {
@@ -115,7 +117,7 @@ namespace SO
                  });
                  Thread.Sleep(1500);
              }
-         }*/
+         }
 
         public void Droga1()
         {
@@ -256,6 +258,161 @@ namespace SO
                 Thread.Sleep(10);
             }
         }
+        public void Droga2()
+        {
+            double r1 = 94;
+            double r2 = 62;
+
+            while (true)
+            {
+                List<Rectangle> rdroga2 = new List<Rectangle>(rauto2);
+                foreach (Rectangle auto2 in rdroga2)
+                {
+
+                    //PIERWSZA PROSTA
+                    if (xa2[rdroga2.IndexOf(auto2)] > 175 && ya2[rdroga2.IndexOf(auto2)] == 477)
+                    {
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            Canvas.SetLeft(auto2, xa2[rdroga2.IndexOf(auto2)]);
+                            xa2[rdroga2.IndexOf(auto2)] = xa2[rdroga2.IndexOf(auto2)] - v2[rdroga2.IndexOf(auto2)];
+
+                            if (rdroga2.IndexOf(auto2) != 0 && (xa2[rdroga2.IndexOf(auto2)] - 80) < (xa2[(rdroga2.IndexOf(auto2)) - 1]))
+                            {
+                                v2[rdroga2.IndexOf(auto2)] = v2[(rdroga2.IndexOf(auto2) - 1)];
+                            }
+
+                            if (xa2[rdroga2.IndexOf(auto2)] == 540 && ya2[rdroga2.IndexOf(auto2)] == 477)
+                            {
+                                if (stop == true)
+                                {
+                                    v2[rdroga2.IndexOf(auto2)] = 0;
+                                }
+                                else
+                                {
+                                    v2[rdroga2.IndexOf(auto2)] = v22[rdroga2.IndexOf(auto2)];
+                                }
+                            }
+                        });
+                    }
+
+                    //PIERWSZY ZAKRET
+                    else if (kat2[rdroga2.IndexOf(auto2)] > -3)
+                    {
+                        kat2[rdroga2.IndexOf(auto2)] = kat2[rdroga2.IndexOf(auto2)] - 0.1;
+                        xa22[rdroga2.IndexOf(auto2)] = xa2[rdroga2.IndexOf(auto2)] + r1 * Math.Sin(kat2[rdroga2.IndexOf(auto2)]);
+                        ya22[rdroga2.IndexOf(auto2)] = ya2[rdroga2.IndexOf(auto2)] + r1 * Math.Cos(kat2[rdroga2.IndexOf(auto2)]);
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            Canvas.SetLeft(auto2, xa22[rdroga2.IndexOf(auto2)] - 25);
+                            Canvas.SetTop(auto2, ya22[rdroga2.IndexOf(auto2)] - 91);
+
+                        });
+                    }
+
+                    //else
+                    //  MessageBox.Show(Convert.ToString(y2[rectanglesD2.IndexOf(autko2)]));
+
+
+                    else if (xa22[rauto2.IndexOf(auto2)] > 158 && xa22[rauto2.IndexOf(auto2)] < 165)
+                    {
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+
+                            xa2[rauto2.IndexOf(auto2)] = xa22[rauto2.IndexOf(auto2)] - 25;
+                            ya2[rauto2.IndexOf(auto2)] = ya22[rauto2.IndexOf(auto2)] - 91;
+
+                            xa22[rauto2.IndexOf(auto2)] = 0;
+                            ya22[rauto2.IndexOf(auto2)] = 0;
+                        });
+                    }
+
+                    //DRUGA PROSTA
+
+                    else if (xa2[rauto2.IndexOf(auto2)] > 161 - 25 && xa2[rauto2.IndexOf(auto2)] < 490 && ya2[rauto2.IndexOf(auto2)] > 383 - 91 && ya2[rauto2.IndexOf(auto2)] < 384 - 91)
+                    {
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            Canvas.SetLeft(auto2, xa2[rauto2.IndexOf(auto2)]);
+                            xa2[rauto2.IndexOf(auto2)] = xa2[rauto2.IndexOf(auto2)] + v2[rauto2.IndexOf(auto2)];
+
+                            if (rauto2.IndexOf(auto2) != 0 && (xa2[rauto2.IndexOf(auto2)]) > (xa2[(rauto2.IndexOf(auto2)) - 1] - 80))
+                            {
+                                v2[rauto2.IndexOf(auto2)] = v2[(rauto2.IndexOf(auto2) - 1)];
+                            }
+
+                            if (xa2[rauto2.IndexOf(auto2)] == 440 && ya2[rauto2.IndexOf(auto2)] > 237 - 62 && ya2[rauto2.IndexOf(auto2)] < 238 - 62)
+                            {
+                                if (stop == true)
+                                {
+                                    v2[rauto2.IndexOf(auto2)] = 0;
+                                }
+                                else
+                                {
+                                    v2[rauto2.IndexOf(auto2)] = v22[rauto2.IndexOf(auto2)];
+                                }
+                            }
+                        });
+
+                    }
+
+
+                    //DRUGI ZAKRET
+                    else if (kat22[rauto2.IndexOf(auto2)] < 3.6)
+                    {
+                        kat22[rauto2.IndexOf(auto2)] = kat22[rauto2.IndexOf(auto2)] + 0.1;
+                        xa22[rauto2.IndexOf(auto2)] = xa2[rauto2.IndexOf(auto2)] + r2 * Math.Sin(kat22[rauto2.IndexOf(auto2)]);
+                        ya22[rauto2.IndexOf(auto2)] = ya2[rauto2.IndexOf(auto2)] + r2 * Math.Cos(kat22[rauto2.IndexOf(auto2)]);
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            Canvas.SetLeft(auto2, xa2[rauto2.IndexOf(auto2)] + 15);
+                            Canvas.SetTop(auto2, ya2[rauto2.IndexOf(auto2)] - 62);
+                        });
+
+
+                    }
+                    //else
+                    //    MessageBox.Show(Convert.ToString(x2[rectanglesD2.IndexOf(autko2)]));
+
+
+
+                    else if (xa22[rauto2.IndexOf(auto2)] > 462 && xa22[rauto2.IndexOf(auto2)] < 464)
+                    {
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+
+                            xa22[rauto2.IndexOf(auto2)] = xa22[rauto2.IndexOf(auto2)] + 15;
+                            ya22[rauto2.IndexOf(auto2)] = ya22[rauto2.IndexOf(auto2)] - 62;
+
+                            xa22[rauto2.IndexOf(auto2)] = 0;
+                            ya22[rauto2.IndexOf(auto2)] = 0;
+                        });
+
+                    }
+
+                    //TRZECIA PROSTA
+                    else if (xa2[rauto2.IndexOf(auto2)] < 464 + 15 && xa2[rauto2.IndexOf(auto2)] > 0 && ya2[rauto2.IndexOf(auto2)] > 237 - 62 && ya22[rauto2.IndexOf(auto2)] < 238 - 62)
+                    {
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            Canvas.SetLeft(auto2, xa22[rauto2.IndexOf(auto2)]);
+                            xa2[rauto2.IndexOf(auto2)] = xa2[rauto2.IndexOf(auto2)] - v2[rauto2.IndexOf(auto2)];
+
+                            if (rauto2.IndexOf(auto2) != 0 && (xa2[rauto2.IndexOf(auto2)]) > (xa2[(rauto2.IndexOf(auto2)) - 1] - 80))
+                            {
+                                v2[rauto2.IndexOf(auto2)] = v2[(rauto2.IndexOf(auto2) - 1)];
+                            }
+                        });
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
         public void Pociag()
         {
             while (true)
@@ -269,7 +426,7 @@ namespace SO
                     Canvas1.Children.Add(train);
                     Canvas.SetBottom(train, -20);
                     Canvas.SetLeft(train, -40);
-                    train.Add(train);
+                    trains.Add(train);
                     yac.Add(0);
                 });
                 Thread.Sleep(25000);
@@ -279,20 +436,20 @@ namespace SO
         {
             while (true)
             {
-                List<Rectangle> train2 = new List<Rectangle>(train);
-                foreach (Rectangle train in train2)
+                List<Rectangle> trains2 = new List<Rectangle>(trains);
+                foreach (Rectangle train in trains2)
                 {
 
                     stop = true;
 
 
-                    if (yac[train2.IndexOf(train)] <= 600)
+                    if (yac[trains2.IndexOf(train)] <= 600)
                     {
                         this.Dispatcher.Invoke(() =>
                         {
-                            Canvas.SetBottom(train, yac[train2.IndexOf(train)]);
-                            yac[train2.IndexOf(train)] = yac[train2.IndexOf(train)] + 3;
-                            if (yac[train2.IndexOf(train)] >= 600)
+                            Canvas.SetBottom(train, yac[trains2.IndexOf(train)]);
+                            yac[trains2.IndexOf(train)] = yac[trains2.IndexOf(train)] + 3;
+                            if (yac[trains2.IndexOf(train)] >= 600)
                             {
                                 stop = false;
                             }
